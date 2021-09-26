97208c4a2619e0a6d82d93eebe3ad52d05f1fbd4
Konstantin Shebalin <championacolironni@gmail.com>
2021-09-27 00:02:24 +0700

    Security_2.1

--- src/main/java/ru/netology/hibernate/Controller.java
+++ src/main/java/ru/netology/hibernate/Controller.java
@@ -1,10 +1,15 @@
 package ru.netology.hibernate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,17 +24,27 @@ public class Controller {
     }
 
     @GetMapping("/persons/by-city")
+    @Secured("ROLE_READ")
     public List<Person> byCity(@RequestParam String city) {
         return service.getPersonByCity(city);
     }
 
     @GetMapping("/persons/age")
+    @RolesAllowed("ROLE_WRITE")
     public List<Person> byAge(@RequestParam int age) {
         return service.getPersonByAge(age);
     }
 
     @GetMapping("/persons/name")
+    @PreAuthorize("hasRole('WRITE') or hasRole('DELETE')")
     public Optional<Person> byNameSurname(@RequestParam String name, @RequestParam String surname) {
         return service.getPersonByNameAndSurname(name, surname);
+    }
+
+
+    @GetMapping("/username")
+    @PostAuthorize("returnObject.username == authentication.principal.username")
+    public String userName(String username) {
+
     }
 }
--- src/main/java/ru/netology/hibernate/SpringSecurityConfig.java
+++ src/main/java/ru/netology/hibernate/SpringSecurityConfig.java
@@ -2,22 +2,24 @@ package ru.netology.hibernate;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("Jhon").password("{noop}9874").roles("Admin");
+        auth.inMemoryAuthentication().withUser("Jhon").password("{noop}9874").roles("READ")
+                .and().withUser("Matt").password("{noop}1234").roles("WRITE")
+                .and().withUser("Scott").password("{noop}666").roles("DELETE");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin().and()
-                .authorizeRequests().antMatchers("/persons/age").permitAll()
-                .and()
                 .authorizeRequests().anyRequest().authenticated();
     }
 }
